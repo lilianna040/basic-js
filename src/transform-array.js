6c843128207afebf18a527aa9f51ef43c093920f
furi.kiri <68464853+lilianna040@users.noreply.github.com>
2020-10-30 01:08:03 +0300

    feat: solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  console.log(arr)
+  let newArr=[];
+  for (let i=0; i<arr.length; i++) {
+    switch (arr[i]) {
+      case '--double-next': arr[i+1]!=undefined ? newArr.push(arr[i+1]) : ''; break;
+      case '--double-prev': (arr[i-2]!='--discard-next' && arr[i-1]!=undefined) ? newArr.push(arr[i-1]) : ''; break;
+      case '--discard-next': i++; break;
+      case '--discard-prev': arr[i-2]!='--discard-next' ? newArr.pop() : ''; break;
+      default : newArr.push(arr[i]); break;
+    }
+  }
+  return newArr;
 };
