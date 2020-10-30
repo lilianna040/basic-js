de4ba41465f481ec81262a00e7c5b99ceac7cd23
furi.kiri <68464853+lilianna040@users.noreply.github.com>
2020-10-30 16:57:36 +0300

    fix: removed extra bracket }

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,9 +3,8 @@ const CustomError = require("../extensions/custom-error");
 module.exports = class DepthCalculator {
    calculateDepth(arr, count = 1, depth=[]) {
     depth.push(count);
-    for (var i=0;i<arr.length;i++){
+    for (var i=0; i<arr.length ;i++){
       if (Array.isArray(arr[i])) this.calculateDepth(arr[i], count+1, depth);
-      }
     }
     return Math.max(...depth);
   }
